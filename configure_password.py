adb13742529a65e70830084acc3b70d5b6bd1ff1
Thiago Marques <thiagomarquesdums@gmail.com>
2024-04-23 11:11:43 -0400

    Update configure_password.py

--- configure_password.py
+++ configure_password.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-
+"""
+Caixas de diálogo para interação com usuários
+"""
+#from pyautogui import alert, confirm, prompt, password
 import shutil
 import pyautogui
 import os
@@ -20,21 +22,21 @@ print(resposta)
 #COLETA NOVA SENHA
 if resposta.upper()[0] == 'S':
     USUARIO = pyautogui.prompt(text='Digite seu usuário: ', title="Informe o usuário")
-    SENHA = pyautogui.password(text='Digite sua senha: ', title="Update Password")
+    #SENHA = pyautogui.password(text='Digite sua senha: ', title="Update Password")
     pyautogui.alert(text='Senha armazenada com sucesso.')
-    pyautogui.alert(text='Reiniciando o sistema!')
+    pyautogui.alert(text='Faça um logoff e inicie novamente!')
 
     #DELETAR ARQUIVO ANTIGO
-    if os.path.exists("inicializa-vmhorizon.sh"):
-        os.remove("inicializa-vmhorizon.sh")
+    if os.path.exists("/home/usuario/HorizonClick/inicializa-vmhorizon.sh"):
+        os.remove("/home/usuario/HorizonClick/inicializa-vmhorizon.sh")
     else:
         print("O arquivo não existe!")
 
     #CRIAR ARQUIVOS DE INICIALIZACAO
-    arquivo = open('inicializa-vmhorizon.sh', "a", encoding='utf-8')
+    arquivo = open('/home/clickti/HorizonClick/inicializa-vmhorizon.sh', "a", encoding='utf-8')
     arquivo.write(f"#!/bin/bash \n")
-    arquivo.write(f"/usr/bin/vmware-view -u {USUARIO} -p {SENHA} -q")
-    os.chmod("inicializa-vmhorizon.sh", 0o777)
-    os.system("reboot")
+    #arquivo.write(f"/usr/bin/vmware-view -u {USUARIO} -p {SENHA} -q")
+    arquivo.write(f"/usr/bin/vmware-view -u {USUARIO} audi.dominio.com.br -q")
+    os.chmod("/home/usuario/HorizonClick/inicializa-vmhorizon.sh", 0o777)
 else:
-    pyautogui.alert('OXII, não vai trabalhar não???')
+    pyautogui.alert(text='Oxi, não vai trabalhar não???')
